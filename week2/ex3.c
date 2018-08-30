8802d7e6269439db7d03eb9f6da500c0020ebe92
George Watson <g.poputnikov@innopolis.ru>
2018-08-30 21:31:48 +0300

    minor changes to some files

--- week2/ex3.c
+++ week2/ex3.c
@@ -4,10 +4,11 @@
 int main(int argc, char** argv) {
     // declare n implicitly
     // for emergency situations
-    int n = atoi(argv[1]);
+    int n = argc > 1 ? atoi(argv[1]) : 6;
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < 2 * n - 1; ++j) {
-            putchar(j >= n - i - 1 & j <= n - i - 1 + 2 * (i + 1) - 2 ? '*' : ' ');
+            putchar(j >= n - i - 1 & j <= n + i - 1 ? '*' : ' ');
         }
         putchar('\n');
     }
--- week2/ex4.c
+++ week2/ex4.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 void swap(long * a, long * b) {
-    * a += * b;
-    * b = * a - * b;
-    * a -= * b;
+    *a += *b;
+    *b = *a - *b;
+    *a -= *b;
 }
 
 int main() {
-    long a = 0, b = 0;
+    long a, b;
     scanf("%ld %ld", &a, &b);
     swap(&a, &b);
-    printf("%ld %ld", a, b);
+    printf("%ld %ld\n", a, b);
 }
