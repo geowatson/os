43d5f95f4b5289355745a847208a964a205e4329
George Watson <g.poputnikov@innopolis.ru>
2018-08-30 11:07:12 +0300

    t

--- s.c
+++ s.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     // for emergency situations
     int n = 1;
     //printf("%s\n", argv[1]);
-    sscanf(n, "%d", argv[1]);
+//    sscanf(n, "%d", argv[1]);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < 2 * n - 1; ++j) {
             putchar(j >= n - i - 1 & j <= n - i - 1 + 2 * (i + 1) - 2 ? '*' : ' ');
