94b85b5f61178d9592899c1fa10d9a9953fd172b
George Watson <g.poputnikov@innopolis.ru>
2018-09-12 23:56:54 +0300

    w4 upd

--- week4/ex4.c
+++ week4/ex4.c
@@ -2,23 +2,61 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
+
+extern char ** environ;
+static char * args[1024];
+static int n = 0;
+
+static char* skipwhite(char* s)
+{
+        while (isspace(*s)) ++s;
+        return s;
+}
+
+static void split(char* cmd)
+{
+        cmd = skipwhite(cmd);
+        char* next = strchr(cmd, ' ');
+        int i = 0;
+
+        while(next != NULL) {
+                next[0] = '\0';
+                args[i] = cmd;
+                ++i;
+                cmd = skipwhite(next + 1);
+                next = strchr(cmd, ' ');
+        }
+
+        if (cmd[0] != '\0') {
+                args[i] = cmd;
+                next = strchr(cmd, '\n');
+                next[0] = '\0';
+                ++i;
+        }
+        n = i - 1;
+
+        args[i] = NULL;
+}
 
 int main() {
-        static char * command;
+        static char command[1024];
+        pid_t pid;
 
         while(1) {
-                printf("simple: shell$ ");
-                command = malloc(sizeof(char) * 1024);
                 fgets(command, 1024, stdin);
-                if (!strcmp(command, "exit\n")) {
+                split(command);
+                if (strcmp(args[0], "exit") == 0) {
                         break;
                 }
-                if (strlen(command) && command[strlen(command) - 1] == '&') {
+                if (strcmp("&", args[n]) == 0) {
                         if (fork() == 0) {
-                                system(command);
+                                char ** args2 = args;
+                                args2[n] = NULL;
+                                execvp(args[0], args);
                         }
                 }
-                else if (strlen(command)) {
+                else {
                         system(command);
                 }
         }
